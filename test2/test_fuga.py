3b38e5f5defa09f7e49adeb02190f3a2854ed334
momohana <ttnarita@gmail.com>
2020-04-22 08:23:47 +0900

    ReadMe追加

--- test2/test_fuga.py
+++ test2/test_fuga.py
@@ -1,8 +1,21 @@
 import unittest
-import fuga
+from fuga import HogeError, fuga
 
 
 class FugaTest(unittest.TestCase):
   # 発生した例外の種類だけチェックすれば十分な場合
+  def test_hoge(self):
+    with self.assertRaises(HogeError):
+      fuga()
+
+    # with使わない場合の書き方。withを使う方がぱっと見てわかりやすい
+    self.assertRaises(HogeError, fuga)
+
+  # 発生した例外のエラーメッセージもチェックしたい場合
   def test_fuga(self):
-    with self.assertRaises()
+    # エラーメッセージのチェックには正規表現が使える
+    with self.assertRaisesRegex(HogeError, ".*1234.*hoge.*"):
+      fuga()
+
+if __name__=='__main__':
+  unittest.main()  
